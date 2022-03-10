ef08babc8d0c07fab8c3e850e49707f4d69ac253
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-03-10 11:21:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,62 @@
-import Card from './components/card/Card'
-import './App.css';
-import json from './news.json'
-import { useState, useEffect } from 'react';
+import Card from "./components/card/Card";
+import axios from "axios";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const news = json.articles
-  const title = json.title
-  const id = json.id
-  const raiting = json.raiting
-  const urlToImage = json.urlToImage
-  const cardsSumma = 15 //количество рендеров карточек
+  const { title, url, urlToImage } = [];
+  const cardsSumma = 15; //количество рендеров карточек
 
   const [card, setCard] = useState(cardsSumma);
-  let renderNews = [...news.slice(0, cardsSumma), ...news.slice(cardsSumma, card)]
-
-
+  const [news, setCards] = useState([]);
+  let renderNews = [
+    ...news.slice(0, cardsSumma),
+    ...news.slice(cardsSumma, card),
+  ];
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
     // eslint-disable-next-line
   }, [card]);
 
-
-
   const handleScroll = (e) => {
-    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
-      setCard(card + 1)
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setCard(card + 1);
     }
   };
 
+  useEffect(() => {
+    const getCards = async () => {
+      const res = await axios.get(
+        "https://nomoreparties.co/news/v2/everything?language=ru&sortBy=publishedAt&pageSize=100&qInTitle=Россия&apiKey=10e8db0981ec4941becf1c27cd92454d"
+      );
+      setCards(res.data.articles);
+    };
+    getCards();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Новости</h1>
-      <h3>Карточки добавляются по скроллу мыши или кнопкой</h3>
-      <button onClick={() => setCard(card + 1)}>Добавить карточки</button>
+      <h1>Новости России</h1>
+      <h3>Карточки добавляются по скроллу мыши</h3>
       <div className="wrapper">
-        {renderNews.map((item, index) =>
-          <Card key={index} item={item} id={id} raiting={raiting} title={title} urlToImage={urlToImage}/>
-        )}
-
+        {renderNews.map((item, index) => (
+          <Card
+            key={index}
+            item={item}
+            title={title}
+            urlToImage={urlToImage}
+            url={url}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
 export default App;
-
-
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,18 +4,18 @@ import './card.css';
 function Card(props) {
   const description = props.item.description
   const title = props.item.title
-  const id = props.item.id
-  const raiting = props.item.raiting
+  const url = props.item.url
   const urlToImage = props.item.urlToImage
 
   return (
     <div className="card">
-      <img src={urlToImage} alt="картинка" />
-      <h4 className="card__title">{title}</h4>
-      <h5 className="card__description">{description}</h5>
-      <div className="card__span">
-        <h6>Номер новости: {id}</h6>
-        <h6>Рейтинг: {raiting}</h6>
+     <img src={urlToImage} alt="картинка" />
+      <div className="card-wrapper">
+        <div className="card__span">  
+        </div>
+        <a href={url}>ссылка</a>
+        <h4 className="card__title">{title}</h4>
+        <h5 className="card__description">{description}</h5>
       </div>
 
 
