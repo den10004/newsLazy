8b7168ec4865e80076a7fefbe69c25c1fb868aac
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-03-15 10:36:58 +0300

    измненён дизайн карточек

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,24 +1,19 @@
-import './card.css';
-
+import "./card.css";
 
 function Card(props) {
-  const description = props.item.description
-  const title = props.item.title
-  const url = props.item.url
-  const urlToImage = props.item.urlToImage
+  const description = props.item.description;
+  const title = props.item.title;
+  const url = props.item.url;
+  const urlToImage = props.item.urlToImage;
 
   return (
     <div className="card">
-     <img src={urlToImage} alt="картинка" />
-      <div className="card-wrapper">
-        <div className="card__span">  
-        </div>
+      <img src={urlToImage} alt="картинка отсутсвует" />
+      <div>
         <a href={url}>ссылка</a>
         <h4 className="card__title">{title}</h4>
         <h5 className="card__description">{description}</h5>
       </div>
-
-
     </div>
   );
 }
